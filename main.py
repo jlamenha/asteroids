42e262f0099b6b61225ac0d1d58712dc0c2293a3
Lucas Lamenha <lamenhajoaolucas@gmail.com>
2025-09-09 01:14:36 -0400

    Shots, asteroids, and movement

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots,updatable,drawable)
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         for event in pygame.event.get():
@@ -12,8 +33,24 @@ def main():
                 return
 
         screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
 
+        dt = clock.tick(60.0) / 1000
 
+        for sprite in updatable:
+            sprite.update(dt)
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if(asteroid.isColliding(shot)):
+                    asteroid.kill()
+                    shot.kill()
+            if(player.isColliding(asteroid)):
+                print("Game over!")
+                return
+
+        
         pygame.display.flip()
 
 if __name__ == "__main__":
