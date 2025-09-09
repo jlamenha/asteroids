c3861c831d32fe1472a6e2380a813eb09314f94d
Lucas Lamenha <lamenhajoaolucas@gmail.com>
2025-09-09 01:23:58 -0400

    splitting and done

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if(asteroid.isColliding(shot)):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
             if(player.isColliding(asteroid)):
                 print("Game over!")
